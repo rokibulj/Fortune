7eaaf8cb8384e51a82bb1fadc18a33b1bf03ecd1
rokibulj <rokibulj2@gmail.com>
2018-03-08 14:47:04 -0500

    Functionality add on Driver, Saucelab, Testbase & ReadProperties class

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -1,5 +1,24 @@
 package base;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 public class TestBase extends SauceLabs{
+	
+	@BeforeSuite
+	public void setUp() {
+		setDriver();
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void sampleTest() {
+		System.out.println(driver.getTitle());
+	}
 
 }
